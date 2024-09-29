c6952bffcfd7a0ec84dc7393b5600c826e0ea20c
Victor Hugo <victorferrarihugo@hotmail.com>
2024-09-29 20:13:21 -0300

    Adição de alguns comentários

--- Algoritmos/QuickSort.c
+++ Algoritmos/QuickSort.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *quickSort(int *ptr, int left, int right) {
+#define TAM 6
+
+int *quickSortC(int *ptr, int left, int right) {
     int i, j, aux, meio;
     /*Lembre-se LEFT é passado como argumento formal da função e o seu valor é 0
     RIGHT também é passado como argumento e no exemplo usado vale 4*/
@@ -28,29 +30,74 @@ int *quickSort(int *ptr, int left, int right) {
     pois não foi feita uma verificação da troca dos elementos, apenas foram trocados de lugar*/
 
     if (left < j) { // Se o j ainda for maior que left (0), então ocorrerá a chamada recursiva da função para que ordene a parte esquerda do vetor
-        quickSort(ptr, left, j);
+        quickSortC(ptr, left, j);
     }
     if (i < right) { // Se o i ainda for menor que right (4), então ocorrerá a chamada recursiva da função para que ordene a parte direita do vetor
-        quickSort(ptr, i, right);
+        quickSortC(ptr, i, right);
+    }
+    /*Ao final dessas chamadas recursivas o vetor estará completamente ordenado*/
+    return ptr;
+}
+
+int *quickSortD(int *ptr, int left, int right) {
+    int i, j, aux, meio;
+    /*Lembre-se LEFT é passado como argumento formal da função e o seu valor é 0
+    RIGHT também é passado como argumento e no exemplo usado vale 4*/
+    i = left; 
+    j = right;
+    meio = ptr[(left + right)/2]; // define o pivo
+
+    do
+    {
+        while (ptr[i] > meio && i < right) i++; // O loop executa até que ache um elemento MAIOR que o "pivô", i irá percorrer toda a parte esquerda antes do pivo
+        while (meio > ptr[j] && j > left) j--; // Depois que o loop de cima termina, esse loop procurará um elemento MENOR que o "pivô", j irá percorrer toda a parte direita depois do pivo
+
+        if (i <= j) { /*Se j for maior que o i, ou seja o elemento na posição i é MAIOR que o da posição J, então precisamos trocá-los de lugar
+        *caso o j fosse menor que o i, isso significa que o elemento já está a esquerda do pivô*/ 
+            aux = ptr[i];
+            ptr[i] = ptr[j];
+            ptr[j] = aux;
+            i++; j--;
+        } // realiza a troca dos elementos e avança os índices, se i ainda for menor que j, volta para os while's
+        
+    } while (i <= j); // O laço continuará até que i supere j, ou seja, i passe do pivo
+
+    /*Depois de realocar os elementos menores a esquerda do pivo e os maiores a direita, ainda é preciso ordená-los,
+    pois não foi feita uma verificação da troca dos elementos, apenas foram trocados de lugar*/
+
+    if (left < j) { // Se o j ainda for maior que left (0), então ocorrerá a chamada recursiva da função para que ordene a parte esquerda do vetor
+        quickSortD(ptr, left, j);
+    }
+    if (i < right) { // Se o i ainda for menor que right (4), então ocorrerá a chamada recursiva da função para que ordene a parte direita do vetor
+        quickSortD(ptr, i, right);
     }
     /*Ao final dessas chamadas recursivas o vetor estará completamente ordenado*/
     return ptr;
 }
 
 int main() {
-    int *p = malloc(5*sizeof(int));
+    int *p = malloc(TAM*sizeof(int));
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < TAM; i++) {
         *(p + i) = rand() % 10;
     }
 
     printf("Vetor:\n");
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < TAM; i++) {
         printf("%d ", *(p + i));
     }
-    p = quickSort(p, 0, 5-1);
-    printf("\nVetor ordenado:\n");
-    for (int i = 0; i < 5; i++) {
+
+    p = quickSortC(p, 0, TAM - 1);
+
+    printf("\nVetor ordenado de forma crescente:\n");
+    for (int i = 0; i < TAM; i++) {
+        printf("%d ", *(p + i));
+    }
+
+    p = quickSortD(p, 0, TAM - 1);
+
+    printf("\nVetor ordenado de forma decrescente:\n");
+    for (int i = 0; i < TAM; i++) {
         printf("%d ", *(p + i));
     }
     return 0;
--- Algoritmos/ShellSort.c
+++ Algoritmos/ShellSort.c
@@ -6,18 +6,22 @@
 int *shellSortC(int vetor[]) {
     for (int gap = TAM/2; gap > 0; gap /= 2) { // Separa o vetor ao "meio" e faz a comparação dos elementos que estão "gap" posições distantes
 
-        for (int i = gap; i < TAM; i++) {
-            int temp = vetor[i];
+        for (int i = gap; i < TAM; i++) { // Esse loop começa do meio do vetor até o final, no caso, começará com i = 3 e vai até i = 5
+            int temp = vetor[i]; // Pega o elemento na posição 3
             int j;
 
-            for (j = i; j >= gap && vetor[j - gap] > temp; j -= gap) { // Se o valor do número a esquerda for maior do que o atual, eles serão trocados de lugar
-                vetor[j] = vetor[j - gap];
+            /*O segundo loop é o que será responsável por fazer "trocas" dos elementos*/
+            for (j = i /*j começa com o valor de i*/; j >= gap && vetor[j - gap] > temp /*Enquanto j for >= 0 e o elemento "gap distante for maior que o temp o loop
+            continuará"*/; j -= gap) {
+                /*Suponha que o vetor seja [10, 5, 9, 1, 8, 4], inicialmente temp = 1, o elemento [j - gap], ou seja, vetor[0] é 10*/
+                vetor[j] = vetor[j - gap];  /*Então o que acontece aqui? O vetor[3] passa a ser 10 e j = 3, mas lembre-se que j é decrementado ao final do for
+                passando a valer 0*/
             }
 
-            vetor[j] = temp;
+            vetor[j] = temp; // Então como j vale 0, insere 1 na posição 0, depois o processo repete para i = 4 e i = 5
 
         }
-    }
+    } // Depois de ter feito para gap = 3, volta-se ao loop inicial e gap passa a valer 1 e repete todo o processo
 
     return vetor;
 }
